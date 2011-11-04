dc368fb5a8a71b78e193c4b20df3f62981f2e915
Tom Benner <tombenner@gmail.com>
2011-11-03 22:39:21 -0400

    Adding some more documentation into example_hooks.php

--- example_hooks.php
+++ example_hooks.php
@@ -1,14 +1,18 @@
 <?php
 
-// Running additional code after a user has updated their profile
+// Modifying views or making custom views
+// The views that are used by default are in wp-content/plugins/front-end-users/views/, but if you want to modify the views,
+// it's recommended that you copy them into wp-content/themes/my-theme/views/ and modify them there. This will allow you to
+// update this plugin without overwriting the views.
 
-add_action('feu_after_update_user', 'after_update_user', 1, 2);
-function after_update_user($user, $post_data) {
-	// Add code here to process any other fields, etc
+add_filter('feu_settings', 'set_custom_feu_views_directory');
+function set_custom_feu_views_directory($settings) {
+	$settings['views_directory'] = get_theme_root().'/'.get_template().'/views/';
+	return $settings;
 }
 
-// Adding additional views
 
+// Setting up custom views
 /*
 	Settings for a view:
 		title	(string)	Title displayed in the menu and at the top of the page
@@ -21,8 +25,9 @@ function after_update_user($user, $post_data) {
 		items	(array)		A list of view keys; these views will be listed in a dropdown submenu below the view title
 								in the menu.
 */
-add_filter('feu_settings', 'change_feu_settings');
-function change_feu_settings($settings) {
+
+add_filter('feu_settings', 'set_custom_feu_views');
+function set_custom_feu_views($settings) {
 	$settings['views'] = array(
 		'index' => array(
 				'title' => 'Home'
@@ -34,12 +39,56 @@ function change_feu_settings($settings) {
 	return $settings;
 }
 
+
+// Setting up custom views and having some of the views ('my-first-settings-page' and 'my-second-settings-page') show up in the
+// FEU menu as subitems for the another view ('settings'):
+
+add_filter('feu_settings', 'set_custom_feu_views');
+function set_custom_feu_views($settings) {
+	$settings['views'] = array(
+		'index' => array(
+				'title' => 'Home'
+			),
+		'settings' => array(
+				'title' => 'Settings',
+				'items' => array('my-first-settings-page', 'my-second-settings-page')
+			),
+		// These will appear as subitems for the 'settings' menu item because they're listed in as the 'items' of 'settings'.
+		// You'd need to create my-first-settings-page.php and my-second-settings-page.php in the views directory.
+		'my-first-settings-page' => array(
+				'title' => 'My First Settings Page'
+			),
+		'my-second-settings-page' => array(
+				'title' => 'My Second Settings Page'
+			)
+	);
+	return $settings;
+}
+
+
+// To display the FEU menu (which isn't displayed by default), modify views/header.php to include feu_menu(), which displays
+// the menu:
+
+if (feu_is_logged_in()) {
+	feu_menu();
+}
+
+
 // Modify the HTML of the FEU menu (which lists FEU views that have 'in_menu' => true)
 
 add_filter('feu_menu', 'change_feu_menu');
 function change_feu_menu($html) {
 	return '';
 }
+
+
+// Running additional code after a user has updated their profile
+
+add_action('feu_after_update_user', 'after_update_user', 1, 2);
+function after_update_user($user, $post_data) {
+	// Add code here to process any other fields, etc
+}
+
 
 // Make the layout of wp-login.php similar to the rest of the site.  To do this, create files
 // like the ones listed below, which can then be included both in header.php/footer.php and here.
